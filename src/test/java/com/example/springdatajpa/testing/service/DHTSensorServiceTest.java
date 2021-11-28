8dd1bd812ade7d4a935111a322c2fbe8a4f311f4
cyebukayire <peacecyebukayire@gmail.com>
2021-11-28 21:07:21 +0200

    Add: Test DHTSensorservices getAll_success

--- src/test/java/com/example/springdatajpa/testing/service/DHTSensorServiceTest.java
+++ src/test/java/com/example/springdatajpa/testing/service/DHTSensorServiceTest.java
@@ -1,22 +1,36 @@
 package com.example.springdatajpa.testing.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.springdatajpa.testing.model.DHTSensor;
 import com.example.springdatajpa.testing.repository.DHTSensorRepository;
 
 @RunWith(SpringRunner .class)
-@DataJpaTest
 public class DHTSensorServiceTest {
 	
-	@Autowired
-	DHTSensorRepository dhtSensorRepository;
+	@Mock
+	DHTSensorRepository dhtSensorRepositoryMock;
+	
+	@InjectMocks
+	DHTSensorService dhtSensorService;
 	
 	@Test
 	public void getAll_success() {
-		
+		when(dhtSensorRepositoryMock.findAll()).thenReturn(Arrays.asList(
+						new DHTSensor(105,23,34,"Kicukiro"), 
+						new DHTSensor(105,45,56,"Nyarugenge")
+						));
+		assertEquals("Kicukiro", dhtSensorService.getAll().get(0).getLocation());
 	}
 }
