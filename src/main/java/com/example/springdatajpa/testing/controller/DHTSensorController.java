b265637fa1b00e266f958d350c37d016be1090ca
cyebukayire <peacecyebukayire@gmail.com>
2021-11-30 09:05:21 +0200

    Add: CRUD in DHTSensor controllers

--- src/main/java/com/example/springdatajpa/testing/controller/DHTSensorController.java
+++ src/main/java/com/example/springdatajpa/testing/controller/DHTSensorController.java
@@ -5,11 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springdatajpa.testing.model.DHTSensor;
+import com.example.springdatajpa.testing.model.dto.DHTSensorDto;
 import com.example.springdatajpa.testing.service.DHTSensorService;
 import com.example.springdatajpa.testing.utils.APIResponse;
 
@@ -33,7 +39,31 @@ public class DHTSensorController {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new APIResponse(false, "Dhtsensor not found"));
 	}
 	
-//	@GetMapping("/")
+	@PostMapping("/save-dhtsensor")
+	public ResponseEntity<?> saveDhtsensor(@RequestBody DHTSensorDto dto) {
+		if(dhtsensorService.existsByLocation(dto.getLocation())) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Dhtsensor already exists");
+		}
+		DHTSensor dhtsensor = dhtsensorService.create(dto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(dhtsensor);		
+	}
+	
+	@PutMapping("/update-dhtsensor")
+	public ResponseEntity<?> updateDhtsensor(@RequestParam int id, @RequestBody DHTSensorDto dto) {
+		DHTSensor dhtsensor = dhtsensorService.getById(id);
+		if(dhtsensor != null) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(dhtsensor);
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Dhtsensor does not exist");
+	}
+	
+	@DeleteMapping("/delete-dhtsensor")
+	public ResponseEntity<?> deleteDhtsensor(@RequestParam int id){
+		DHTSensor dhtsensor = dhtsensorService.deleteDhtsensor(id);
+		return ResponseEntity.ok(dhtsensor);
+	}
+	
+	
 	
 	
 	
